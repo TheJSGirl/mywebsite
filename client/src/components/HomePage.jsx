38f13ab3df16065e82e695f957d1a5aaa305ca7e
pnegi <prinegi100@gmail.com>
2017-10-11 15:45:04 +0530

    edit HomePage

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -17,15 +17,11 @@ const Home = () => {
       </div>
       <div className="row">
         <div className="s10 offset-s1 m8 offset-m2">
-          <p>
-            <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa placeat molestiae perspiciatis, assumenda molestias, ab. Nam laboriosam ab, eaque quas hic praesentium aliquid sunt, aliquam dignissimos ut accusantium deleniti tempora!</span>
-            <span>Beatae autem temporibus nulla repellat consectetur, deserunt voluptates, modi eos numquam quibusdam ex, tempore eaque impedit quasi explicabo iste error accusamus, aspernatur tenetur enim dolor reiciendis sint laboriosam saepe. Exercitationem!</span>
-            <span>Dolor ipsa consequatur ad fugit repudiandae, quibusdam illum vel quos similique voluptatibus inventore necessitatibus pariatur ab rerum corporis nobis sapiente fuga reprehenderit debitis, harum doloremque in doloribus. Quibusdam, aliquam, ut.</span>
-            <span>Totam, suscipit. Officiis, vitae, voluptate. Molestias deleniti, ipsa sequi libero ullam tenetur ut, quas fugiat consequatur iure id sed dolore? Facilis iusto unde repellat nam a quisquam ex numquam officiis!</span>
-            <span>Itaque commodi quasi reiciendis autem voluptate obcaecati, nobis incidunt in mollitia quos consequatur, totam vel cumque. Consectetur culpa possimus, laborum molestias ea numquam libero repellendus nemo sed vero sit quod.</span>
-            <span>Quod id reiciendis error, quos, modi minima ab illo, unde voluptatum numquam placeat atque! Repudiandae reprehenderit officia et deserunt officiis, fuga quae ad quia nemo, facilis libero recusandae cupiditate suscipit.</span>
-            <span>Eius atque aspernatur error iusto doloremque incidunt vero accusamus cupiditate molestiae! Inventore quasi illo voluptatibus, iure ipsa mollitia sequi, facere sapiente, officia consequatur vitae aperiam quas voluptatum rerum. Esse, amet!</span>
-            <span>Sint minima excepturi aperiam facere sunt, quasi nihil et eaque sapiente eius, magni fugiat ea, laborum aliquid nemo dolore veritatis animi, sit asperiores consequuntur debitis est autem velit. Quis, optio?</span>
+           
+          <h1 className= "center"> <span style={{fontSize: 50}}>Hello there!</span></h1>
+          <p className= "center" style={{fontSize: 20}}>
+            I am Priyanka Negi. I am Javascript Developer.
+            
           </p>
           <br/>
         </div>
