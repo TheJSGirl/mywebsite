4291f8c2302a1ce8234f61c87e7529fcc8907099
Priyanka Negi <prinegi100@gmail.com>
2017-10-15 14:25:12 +0530

    completed GET  /messages

--- routes/messages.js
+++ routes/messages.js
@@ -3,6 +3,42 @@ const Message = require('../models/Message');
 
 
 messages.route('/')
+  .get(async (req, res) => {
+    try{
+          const sentMessage = await Message.find();
+          if(!sentMessage){
+            return res.status(404).json({
+              data: [],
+              status: 'failed',
+              message: 'not found'
+            });
+          }
+          let data = [];
+
+          sentMessage.forEach((element) => {
+            const senderData = {
+                senderName: element.senderName,
+                senderEmail: element.senderEmail,
+                sentMessage: element.senderMessage
+              }
+              data.push(senderData);
+            });
+
+            
+            return res.status(200).json({
+              data: data,
+              status: 'ok',
+              message: 'Successful'
+            });
+      } 
+    catch(err){
+      return res.status(500).json({
+        data: [],
+        status: 'failed',
+        message: 'something went wrong'
+      });
+    }
+  })
   .post(async (req, res) => {
     try {
           console.log(req.body);
@@ -31,9 +67,13 @@ messages.route('/')
     
     catch(err){
         return res.status(500).json({
-
-        })
+          data: [],
+          status: 'failed',
+          message: 'something went wrong'
+        });
     }
   });
+
+
 
     module.exports = messages;
